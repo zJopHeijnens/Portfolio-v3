03ea2ccfd5cc7b2e5d197492c3764931111c0ced
Jop Heijnens <j.heijnens@student.fontys.nl>
2024-04-16 09:57:57 +0200

    Added a 3d renderer file i had made before with the help of a tutorial

---
+++ js/main.js
@@ -0,0 +1,77 @@
+//Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+//Create a Three.JS Scene
+const scene = new THREE.Scene();
+//create a new camera with positions and angles
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 0.5);
+//Keep the 3D object on a global variable so we can access it later
+let object;
+//Instantiate a loader for the .gltf file
+const loader = new GLTFLoader();
+//Load the file
+loader.load(
+  `models/Asset1.gltf`,
+  function (gltf) {
+    //If the file is loaded, add it to the scene
+    object = gltf.scene;
+    scene.add(object);
+  },
+  function (xhr) {
+    //While it is loading, log the progress
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    //If there is an error, log it
+    console.error(error);
+  }
+);
+
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.enableZoom = false
+controls.enablePan = false
+
+
+//Add the renderer to the DOM
+document.getElementById("container3D").appendChild(renderer.domElement);
+
+//Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1); // (skyColor, groundColor, intensity)
+topLight.position.set(500, 500, 500) //top-left-ish
+scene.add(topLight);
+
+
+//Add a listener to the window, so we can resize the window and the camera
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Rotate the object
+const clock = new THREE.Clock();
+
+function animate() {
+  requestAnimationFrame(animate);
+  
+  const elapsedTime = clock.getElapsedTime();
+  if (object) {
+    object.rotation.y = elapsedTime * 1.25; // Adjust the rotation speed here
+  }
+  
+  renderer.render(scene, camera);
+}
+
+//Start the 3D rendering
+animate();
+
